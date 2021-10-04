cf8398d13039f143f17657f3536b35fabe47a3a1
Svir_fz18 <Svir_fz18@student.itstep.org>
2021-10-04 20:11:28 +0300

    Created courses queries and commands

---
+++ DAL/Commands/CourseCommand/ICourseCommand.cs
@@ -0,0 +1,17 @@
+﻿using DAL.DTO;
+using DAL.Entities;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Commands.CourseCommand
+{
+    public interface ICourseCommand
+    {
+        public Course CreateCourse(CourseDTO course);
+
+        public Course ChangeCourse(int courseId, CourseDTO course);
+
+        public void DeleteCourse(int courseId);
+    }
+}
---
+++ DAL/Queries/CourseQuery/ICourseQuery.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Queries
+{
+    public interface ICourseQuery
+    {
+        public IQueryable<Course> GetCourses();
+
+        public Course GetCourseById(int id);
+
+        public Course GetCourseByTitle(string title);
+    }
+}
