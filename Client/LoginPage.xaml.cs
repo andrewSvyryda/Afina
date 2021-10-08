91b197fb881953819fcc494ac457525b867837f0
UlianaKov-hab <a_k_ula@ukr.net>
2021-10-08 05:51:28 +0300

    Add login page

--- Client/LoginPage.xaml.cs
+++ Client/LoginPage.xaml.cs
@@ -10,23 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Client
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginPage : Window
     {
-        LoginPage page;
-        public MainWindow()
+        public LoginPage()
         {
             InitializeComponent();
-            page = new LoginPage();
-            page.Show();
-            this.Close();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
