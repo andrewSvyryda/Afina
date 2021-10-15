7bf74ae8d7f990dc5a46601c7b40649ea957f155
AndriySvyryda <andrijsvirida2@gmail.com>
2021-10-15 12:39:22 +0300

    Finished

---
+++ Server/CourseService.svc.cs
@@ -0,0 +1,44 @@
+﻿using DAL.Commands;
+using DAL.Queries;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Server
+{
+    public class CourseService : ICourseService
+    {
+        public void ChangeCourse(int courseId, CourseDTO course)
+        {
+            CourseCommand courseCommand = new CourseCommand();
+
+            courseCommand.ChangeCourse(courseId, course);
+        }
+
+        public void CreateCourse(CourseDTO course)
+        {
+            CourseCommand courseCommand = new CourseCommand();
+
+            courseCommand.CreateCourse(course);
+        }
+
+        public bool DeleteCourse(int courseId)
+        {
+            CourseCommand courseCommand = new CourseCommand();
+
+            return courseCommand.DeleteCourse(courseId);
+        }
+
+        public List<CourseDTO> GetCourses()
+        {
+            CourseQuery courseQuery = new CourseQuery();
+
+            return AfinaMapper.Mapper.Map<List<CourseDTO>>(courseQuery.GetCourses().ToList());
+        }
+    }
+}
